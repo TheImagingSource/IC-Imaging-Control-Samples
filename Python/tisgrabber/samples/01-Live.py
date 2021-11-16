ade76e5078872b075ef64183fb047c2103c4c47a
Geissler <stefan.geissler@theimagingsource.com>
2021-11-16 16:13:39 +0100

    Reduced the 01-live sample to use of tisgrabber.py, added new simple get an image callback on trigger sample.

--- Python/tisgrabber/samples/01-Live.py
+++ Python/tisgrabber/samples/01-Live.py
@@ -1,36 +1,27 @@
 '''
-This sample demonstrates, how to open a camera with the 
+This sample demonstrates, how to open a camera with the
 built in dialog and show a live video stream.
-Needed DLLs for 64 bit environment are 
+It is changed for import of tisgrabber.py file.
+Needed DLLs for 64 bit environment are
 - tisgrabber_x64.dll
 - TIS_UDSHL11_x64.dll
+- tisgrabber.py
 '''
-import ctypes 
+import ctypes
+import tisgrabber as tis
 
 ic = ctypes.cdll.LoadLibrary("./tisgrabber_x64.dll")
-
-class HGRABBER(ctypes.Structure):
-    '''
-    This class is used to handle the pointer to the internal 
-    Grabber class, which contains the camera. 
-    A pointer to this class is used by tisgrabber DLL.
-    '''
-    _fields_ = [('unused', ctypes.c_int)]
-
-
-# Functions returning a HGRABBER Handle must set their restype to POINTER(HGRABBER)
-ic.IC_ShowDeviceSelectionDialog.restype = ctypes.POINTER(HGRABBER)
-ic.IC_ReleaseGrabber.argtypes = (ctypes.POINTER(ctypes.POINTER(HGRABBER)),)
+tis.declareFunctions(ic)
 
 ic.IC_InitLibrary(0)
 
 hGrabber = ic.IC_ShowDeviceSelectionDialog(None)
- 
-if( ic.IC_IsDevValid(hGrabber)): 
-    ic.IC_StartLive(hGrabber,1)
-    ic.IC_MsgBox( "Click OK to stop".encode("utf-8"),"Simple Live Video".encode("utf-8"))
+
+if(ic.IC_IsDevValid(hGrabber)):
+    ic.IC_StartLive(hGrabber, 1)
+    ic.IC_MsgBox(tis.T("Click OK to stop"), tis.T("Simple Live Video"))
     ic.IC_StopLive(hGrabber)
 else:
-    ic.IC_MsgBox("No device opened".encode("utf-8"), "Simple Live Video".encode("utf-8"),)
+    ic.IC_MsgBox(tis.T("No device opened"), tis.T("Simple Live Video"))
 
 ic.IC_ReleaseGrabber(hGrabber)
---
+++ Python/tisgrabber/samples/32-trigger-callback.py
@@ -0,0 +1,74 @@
+"""
+This samples shows how to enable trigger mode, use software trigger
+and how to use a callback, that is called automatically for each
+incoming image on a trigger signal.
+"""
+import ctypes 
+import tisgrabber as tis
+import cv2 as cv2
+import numpy as np
+
+
+ic = ctypes.cdll.LoadLibrary("./tisgrabber_x64.dll")
+tis.declareFunctions(ic)
+
+ic.IC_InitLibrary(0)
+
+hGrabber = ic.IC_CreateGrabber()
+
+
+class CallbackUserdata(ctypes.Structure):
+    """ Example for user data passed to the callback function. 
+    """
+    def __init__(self, ):
+        self.unsused = ""
+
+
+def frameReadyCallback(hGrabber, pBuffer, framenumber, pData):
+    Width = ctypes.c_long()
+    Height = ctypes.c_long()
+    BitsPerPixel = ctypes.c_int()
+    colorformat = ctypes.c_int()
+
+    # Query the image description values
+    ic.IC_GetImageDescription(hGrabber, Width, Height, BitsPerPixel,
+                              colorformat)
+
+    # Calculate the buffer size
+    bpp = int(BitsPerPixel.value/8.0)
+    buffer_size = Width.value * Height.value * bpp
+
+    if buffer_size > 0:
+        image = ctypes.cast(pBuffer,
+                            ctypes.POINTER(
+                                ctypes.c_ubyte * buffer_size))
+
+        cvMat = np.ndarray(buffer=image.contents,
+                           dtype=np.uint8,
+                           shape=(Height.value,
+                                  Width.value,
+                                  bpp))
+
+        # Do something with cvMat here.
+
+# Manage the callbacks
+# Create the function pointer.
+frameReadyCallbackfunc = ic.FRAMEREADYCALLBACK(frameReadyCallback)
+userdata = CallbackUserdata()
+
+hGrabber = ic.IC_ShowDeviceSelectionDialog(None)
+
+if ic.IC_IsDevValid(hGrabber):
+    ic.IC_SetFrameReadyCallback(hGrabber, frameReadyCallbackfunc, userdata)
+    ic.IC_SetContinuousMode(hGrabber, 0)
+    ic.IC_SetPropertySwitch(hGrabber, tis.T("Trigger"), tis.T("Enable"), 1)
+    ic.IC_StartLive(hGrabber, 1)
+    ic.IC_MsgBox(tis.T("Click OK to trigger"), tis.T("Simple Live Video"))
+    ic.IC_PropertyOnePush(hGrabber, tis.T("Trigger"), tis.T("Software Trigger"))
+
+    ic.IC_MsgBox(tis.T("Click OK to stop"), tis.T("Simple Live Video"))
+    ic.IC_StopLive(hGrabber)
+else:
+    ic.IC_MsgBox(tis.T("No device opened"), tis.T("Simple Live Video"))
+
+ic.IC_ReleaseGrabber(hGrabber)
